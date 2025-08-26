be5d8e3d5a0bfe97e22d2012f669e76e69a578e0
Jim Koch <jimkoch52@gmail.com>
2025-08-26 14:49:09 -0700

    comments

--- src/services/users_service.ts
+++ src/services/users_service.ts
@@ -1,6 +1,12 @@
 import * as usersDao from "../daos/users_dao.ts";
+import { User } from "../types.ts";
 
-export const getUserByUsername = async (username: string) => {
+/**
+ * Gets the user data for the user with the matching username value.
+ * @param {string} username - a username string
+ * @returns {User} a Promise, resolving to User object if the user exists, otherwise null
+ */
+export const getUserByUsername = async (username: string): Promise<User | null> => {
   try {
     return usersDao.findByUsername(username);
   } catch (err: any) {
