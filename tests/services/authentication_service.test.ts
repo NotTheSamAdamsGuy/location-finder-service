f8f9e4ebbdd30b274194661929536061ce2fc3f2
Jim Koch <jimkoch52@gmail.com>
2025-08-27 13:06:07 -0700

    tests for authentication service

---
+++ tests/services/authentication_service.test.ts
@@ -0,0 +1,46 @@
+import { expect, vi, describe, it } from "vitest";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+import { generateToken } from "../../src/services/authentication_service";
+import { User } from "../../src/types";
+
+vi.mock("../../src/services/users_service", () => ({
+  getUserByUsername: (username: string) => {
+    if (username === "testuser") {
+      const user: User = {
+        username: "testuser",
+        password: "password",
+        firstName: "Test",
+        lastName: "User",
+        role: "USER",
+        lastLoginTimestamp: undefined,
+      };
+
+      return user;
+    } else {
+      return null;
+    }
+  },
+}));
+
+describe("AuthenticationService", () => {
+  describe("generateToken", () => {
+    it("should return a token for a valid user", async () => {
+      const token = await generateToken("testuser", "password");
+      expect(token).not.toBe(null);
+
+      const decoded: JwtPayload = jwt.verify(
+        token,
+        process.env.JWT_SECRET_KEY as string
+      ) as JwtPayload;
+
+      expect(decoded.username).toEqual("testuser");
+    });
+
+    it("should throw an error for an invalid user", async () => {
+      await expect(generateToken("testuser2", "password")).rejects.toThrowError(
+        "Invalid credentials"
+      );
+    });
+  });
+});
